582164294f8d93b9f22228fe8fb824bb9b1e0696
MEHAMMEDAMINE <48122920+MEHAMMEDAMINE@users.noreply.github.com>
2022-03-20 09:32:39 +0100

    Update run_asc.py

--- pytorch-pretrained-bert/src/run_asc.py
+++ pytorch-pretrained-bert/src/run_asc.py
@@ -205,7 +205,7 @@ def main():
     parser.add_argument("--bert_model", default='bert-base', type=str)
 
     parser.add_argument("--data_dir",
-                        default="data",
+                        default="data/news",
                         type=str,
                         required=False,
                         help="The input data dir containing json files.")
